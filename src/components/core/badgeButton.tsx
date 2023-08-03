9f8a83391a5238fcf7020c3c454e04e4500a6203
UDay <uday.dev.bd@gmail.com>
2023-08-04 00:14:30 +0600

    design badge button

---
+++ src/components/core/badgeButton.tsx
@@ -0,0 +1,23 @@
+import { ReactNode } from "react";
+
+interface IBadgeButtonProps {
+  Icon: any;
+  label?: string;
+  subTitle?: string;
+}
+export const BadgeButton = ({ Icon, label, subTitle }: IBadgeButtonProps) => {
+  return (
+    <button className="flex gap-1 items-center">
+      <div className="relative">
+        <Icon className="w-8 aspect-square" />
+        <div className="bg-primary rounded-full w-4 h-4 absolute top-0 right-0 text-[.6rem] text-white flex justify-center items-center">
+          9
+        </div>
+      </div>
+      <div className="flex flex-col text-xs items-start">
+        <span className="">{label}</span>
+        <span className="text-primary text-[.6rem] ">{subTitle}</span>
+      </div>
+    </button>
+  );
+};
