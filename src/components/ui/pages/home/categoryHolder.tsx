3af7f0e4b16451317c6dc749ee7f25efd0861001
UDay <uday.dev.bd@gmail.com>
2023-08-04 23:21:16 +0600

    fetch data

---
+++ src/components/ui/pages/home/categoryHolder.tsx
@@ -0,0 +1,29 @@
+import { CategoryCard } from "../../categoryCard";
+
+interface ICategory {
+  name: string;
+  url: string;
+  itemCount: number;
+  color: string;
+}
+
+async function getCategories<T>(): Promise<T[]> {
+  const res = await fetch(`${process.env.BASE_URL}/api/category`);
+  return res.json();
+}
+
+export const CategoryHolder = async () => {
+  const categories: ICategory[] = await getCategories<ICategory>();
+  return (
+    <div className="container mx-auto py-10 px-5 xl:px-0">
+      <div>
+        <h4 className="text-2xl py-7">Explore Categories</h4>
+      </div>
+      <div className="flex gap-5 flex-wrap">
+        {categories.map((item, index) => (
+          <CategoryCard key={index} {...item} />
+        ))}
+      </div>
+    </div>
+  );
+};
