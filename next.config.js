fef638c2907b81e79e3caa3efd5a95239dd21173
UDay <uday.dev.bd@gmail.com>
2023-08-06 01:21:32 +0600

    add config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  env: {
+    BASE_URL:
+      process.env.NODE_ENV == "development"
+        ? "http://localhost:3000"
+        : `https://${process.env.VERCEL_URL}`,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
