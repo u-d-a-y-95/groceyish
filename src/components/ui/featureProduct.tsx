d34307d9107c872fbce13faa6a16a5ec14a2a37c
UDay <uday.dev.bd@gmail.com>
2023-08-06 02:55:00 +0600

    stop server component

--- src/components/ui/featureProduct.tsx
+++ src/components/ui/featureProduct.tsx
@@ -1,26 +1,26 @@
 import { IProduct } from "@/helper/dtos/product.dto";
 import { ProductCard } from "./ProductCard";
 
-async function getFeatureProducts<T>(): Promise<T[]> {
-  const res = await fetch(`${process.env.BASE_URL}/api/product/features`, {
-    next: {
-      tags: ["FeaturedProduct"],
-    },
-  });
-  return res.json();
-}
+// async function getFeatureProducts<T>(): Promise<T[]> {
+//   const res = await fetch(`${process.env.BASE_URL}/api/product/features`, {
+//     next: {
+//       tags: ["FeaturedProduct"],
+//     },
+//   });
+//   return res.json();
+// }
 
 export const FeatureProducts = async () => {
-  const products: IProduct[] = await getFeatureProducts<IProduct>();
+  // const products: IProduct[] = await getFeatureProducts<IProduct>();
   return (
     <div className="container mx-auto py-5 px-5 xl:px-0">
       <div>
         <h4 className="text-2xl py-7">Featured Products</h4>
       </div>
       <div className="grid grid-cols-1  md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-5  justify-items-center md:justify-items-start">
-        {products.map((item, index) => (
+        {/* {products.map((item, index) => (
           <ProductCard key={index} {...item} />
-        ))}
+        ))} */}
       </div>
     </div>
   );
--- src/components/ui/pages/home/categoryHolder.tsx
+++ src/components/ui/pages/home/categoryHolder.tsx
@@ -7,27 +7,27 @@ interface ICategory {
   bgColor: string;
 }
 
-async function getCategories<T>(): Promise<T[]> {
-  const res = await fetch(`${process.env.BASE_URL}/api/category`, {
-    next: {
-      tags: ["Category"],
-    },
-  });
-  return res.json();
-}
+// async function getCategories<T>(): Promise<T[]> {
+//   const res = await fetch(`${process.env.BASE_URL}/api/category`, {
+//     next: {
+//       tags: ["Category"],
+//     },
+//   });
+//   return res.json();
+// }
 
 export const CategoryHolder = async () => {
-  const categories = await getCategories<ICategory>();
+  // const categories = await getCategories<ICategory>();
   return (
     <div className="container mx-auto py-10 px-5 xl:px-0">
       <div>
         <h4 className="text-2xl py-7">Explore Categories</h4>
       </div>
-      <div className="flex gap-5 flex-wrap">
+      {/* <div className="flex gap-5 flex-wrap">
         {categories.map((item, index) => (
           <CategoryCard key={index} {...item} />
         ))}
-      </div>
+      </div> */}
     </div>
   );
 };
